e2ea991f87967537503feb1c182cd19210c81663
ivanpalacios2809@gmail.com <ivanpalacios2809@gmail.com>
2024-01-29 15:49:49 -0600

    Refactor Button component to use onClick prop

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -4,32 +4,30 @@ interface Props {
     type: ButtonType;
     value: string;
     url?: string;
+    onClick?: () => void;
 }
 
-export default function Button({ type, value, url }: Props): JSX.Element {
+export default function Button({ type, value, url, onClick }: Props): JSX.Element {
+    const button = (
+        <button
+            className="block w-full transform rounded-md bg-primary px-4 py-2 text-center font-medium
+            capitalize tracking-wide text-white transition-colors duration-300 hover:bg-hover
+            focus:outline-none focus:ring focus:ring-hover focus:ring-opacity-80"
+            type={type}
+            onClick={onClick}
+        >
+            {value}
+        </button>
+    );
     return (
         <>
             {
                 url ? (
                     <a href={url} >
-                        <button
-                            className="block w-full transform rounded-md bg-primary px-4 py-2 text-center font-medium
-    capitalize tracking-wide text-white transition-colors duration-300 hover:bg-hover
-    focus:outline-none focus:ring focus:ring-hover focus:ring-opacity-80"
-                            type={type}
-                        >
-                            {value}
-                        </button>
+                        {button}
                     </a>
                 ) : (
-                    <button
-                        className="block w-full transform rounded-md bg-primary px-4 py-2 text-center font-medium
-  capitalize tracking-wide text-white transition-colors duration-300 hover:bg-hover
-  focus:outline-none focus:ring focus:ring-hover focus:ring-opacity-80"
-                        type={type}
-                    >
-                        {value}
-                    </button>
+                    button
                 )
             }
         </>

9cf2457219e10874a4018063425ee5e12a131a1d
ivanpalacios2809@gmail.com <ivanpalacios2809@gmail.com>
2024-01-29 15:41:27 -0600

    Refactor AcceptUsersPage component and add user actions

--- src/app/admin/accept-users/page.tsx
+++ src/app/admin/accept-users/page.tsx
@@ -1,14 +1,39 @@
+"use client"
 import CenterContainer from "@/app/components/containers/CenterContainer"
 import Table from "@/app/components/ui/Table"
-
-export default async function AcceptUsersPage() {
-    const response = await fetch("http://localhost:3000/api/pending-users")
-    const data = await response.json()
-    console.log(data)
+import { ButtonType } from "../../../../types/types"
+import { useState } from "react"
+import { useEffect } from "react"
+const actions = [
+    {
+        value: "Aceptar",
+        onClick: () => {
+            console.log("Accept")
+        },
+        type: "button" as ButtonType
+    },
+    {
+        value: "Rechazar",
+        onClick: () => {
+            console.log("Reject")
+        },
+        type: "button" as ButtonType
+    }
+]
+export default function AcceptUsersPage() {
+    const [users, setUsers] = useState()
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch("http://localhost:3000/api/pending-users")
+            const data = await response.json()
+            setUsers(data.data)
+        }
+        fetchUsers()
+    }, [])
     return (
         <>
             <CenterContainer>
-                <Table data={data.data} />
+                <Table data={users} actions={actions} />
             </CenterContainer>
         </>
     )

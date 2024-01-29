251c955631bc7dbaf6becd5d7483db86419a0b34
ivanpalacios2809@gmail.com <ivanpalacios2809@gmail.com>
2024-01-29 15:49:20 -0600

    Add setKeys function to set unique keys for data (WIP, beta feature)

--- src/app/api/pending-users/route.ts
+++ src/app/api/pending-users/route.ts
@@ -1,12 +1,13 @@
 import { connectDB } from "@/utils/mongoose";
 import setFriendlyHeadings from "@/utils/setFriendlyHeadings"
-
+import setKeys from "@/utils/setKeys"
 import Alumno from "@/utils/models/Alumno";
 import { NextResponse } from "next/server";
 export async function GET(request: Request) {
     await connectDB();
     const alumnos_pending = await Alumno.find({ status: 'pendiente' }, { password: 0, __v: 0, _id: 0 })
     const newData = setFriendlyHeadings(alumnos_pending);
+    const dataWithKeys = setKeys(newData, 'Correo')
     console.log(newData)
     return NextResponse.json({ data: newData }, { status: 200 })
 }

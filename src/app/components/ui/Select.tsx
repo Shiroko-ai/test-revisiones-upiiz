1ef8ab29f8949b11278595906825c6917bf46985
Shiroko <ivanpalacios2809@gmail.com>
2024-02-11 16:41:07 -0600

    Update Select component to use defaultValue prop instead of selected prop

--- src/app/components/ui/Select.tsx
+++ src/app/components/ui/Select.tsx
@@ -16,8 +16,8 @@ export default function Select({ label, id, children, name, ...rest }: SelectPro
             <select id={id}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
          focus:ring-primary focus:border-primary block w-full p-2.5 
-          mb-6" name={name}>
-                <option selected disabled>Selecciona una opción</option>
+          mb-6" name={name} defaultValue={''}>
+                <option value="" disabled>Selecciona una opción</option>
                 {children}
             </select>
         </>

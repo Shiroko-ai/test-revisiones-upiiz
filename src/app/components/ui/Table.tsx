c08b7f7af8a6dc7361f3ca18fef2138e7e49b26a
Shiroko <ivanpalacios2809@gmail.com>
2024-02-11 16:41:14 -0600

    Refactor Table component to include a name prop and improve table structure

--- src/app/components/ui/Table.tsx
+++ src/app/components/ui/Table.tsx
@@ -11,61 +11,64 @@ interface Action {
 interface TableProps {
     data: any;
     actions?: Action[];
-
+    name?: string;
 }
 
 import Button from "./Button";
 
 
-export default function Table({ data, actions }: TableProps): JSX.Element {
+export default function Table({ data, actions, name }: TableProps): JSX.Element {
     if (!data) return (<></>)
     return (
-        <table className="max-w-[50%]">
-            <thead className="bg-gray-200 border-b">
-                <tr>
+        <>
+            <h1 className="text-2xl font-bold text-gray-900">{name}</h1>
+            <table className="max-w-[50%] w-full">
+                <thead className="bg-gray-200 border-b">
+                    <tr>
+                        {
+                            Object.keys(data[0]).map((key: string) => (
+                                <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left" key={key}>
+                                    {key}
+                                </th>
+                            ))
+                        }
+                        {
+                            actions && (
+                                <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Acciones</th>
+                            )
+                        }
+
+                    </tr>
+                </thead>
+                <tbody>
                     {
-                        Object.keys(data[0]).map((key: string) => (
-                            <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left" key={key}>
-                                {key}
-                            </th>
+                        data.map((item: any) => (
+                            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100" key={item.Boleta}>
+                                {Object.entries(item).map(([key, value]) => (
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900" key={key}>
+                                        {value as ReactNode}
+                                    </td>
+                                ))}
+                                {actions && (
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <div className="flex flex-row space-x-2">
+                                            {actions.map((action, index) => (
+                                                <Button
+                                                    key={index}
+                                                    value={action.value}
+                                                    type={action.type}
+                                                    url={action.url}
+                                                    onClick={() => action.onClick?.(item.key)}
+                                                />
+                                            ))}
+                                        </div>
+                                    </td>)}
+
+                            </tr>
                         ))
                     }
-                    {
-                        actions && (
-                            <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Acciones</th>
-                        )
-                    }
-
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data.map((item: any) => (
-                        <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100" key={item.Boleta}>
-                            {Object.entries(item).map(([key, value]) => (
-                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900" key={key}>
-                                    {value as ReactNode}
-                                </td>
-                            ))}
-                            {actions && (
-                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                    <div className="flex flex-row space-x-2">
-                                        {actions.map((action, index) => (
-                                            <Button
-                                                key={index}
-                                                value={action.value}
-                                                type={action.type}
-                                                url={action.url}
-                                                onClick={() => action.onClick?.(item.key)}
-                                            />
-                                        ))}
-                                    </div>
-                                </td>)}
-
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </>
     )
 }

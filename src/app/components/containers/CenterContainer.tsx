64f89bb40323a481ba352b9597ca9c534b12c4f9
Shiroko <ivanpalacios2809@gmail.com>
2024-02-11 16:40:56 -0600

    Refactor CenterContainer positioning

--- src/app/components/containers/CenterContainer.tsx
+++ src/app/components/containers/CenterContainer.tsx
@@ -1,6 +1,6 @@
 export default function CenterContainer({ children }: { children: React.ReactNode }): JSX.Element {
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             {children}
         </div>
     )

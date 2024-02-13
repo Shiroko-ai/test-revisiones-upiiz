a65ff19dc28be24eb0d75b626cfd71e1e171adb6
Shiroko <ivanpalacios2809@gmail.com>
2024-02-13 17:32:46 -0600

    WIP: Fixed typing

--- src/app/api/pending-users/route.ts
+++ src/app/api/pending-users/route.ts
@@ -1,13 +1,11 @@
-import { connectDB } from "@/utils/mongoose";
-import setFriendlyHeadings from "@/utils/setFriendlyHeadings"
-import setKeys from "@/utils/setKeys"
-import Alumno from "@/utils/models/Alumno";
-import { NextResponse } from "next/server";
-export async function GET(request: Request) {
-    await connectDB();
-    const alumnos_pending = await Alumno.find({ status: 'pendiente' }, { password: 0, __v: 0, _id: 0 })
-    const newData = setFriendlyHeadings(alumnos_pending);
-    const dataWithKeys = setKeys(newData, 'Correo')
-    console.log(newData)
-    return NextResponse.json({ data: newData }, { status: 200 })
+import { connectDB } from '@/utils/mongoose'
+import setFriendlyHeadings from '@/utils/setFriendlyHeadings'
+import Student from '@/utils/models/Student'
+import { NextResponse } from 'next/server'
+export async function GET (request: Request): Promise<NextResponse> {
+  await connectDB()
+  const pendingStudents: Array<Record<string, string>> = await Student.find({ status: 'pending' }, { password: 0, __v: 0, _id: 0 })
+  const newData = setFriendlyHeadings(pendingStudents)
+  console.log(newData)
+  return NextResponse.json({ newData }, { status: 200 })
 }

a4991610fe8598310fe56e53a0c855caea06926b
Shiroko <ivanpalacios2809@gmail.com>
2024-02-13 17:33:01 -0600

    Refactor register-alumno route.ts

--- src/app/api/register-alumno/route.ts
+++ src/app/api/register-alumno/route.ts
@@ -1,34 +1,35 @@
-import { connectDB } from "../../../utils/mongoose"
-import validateFields from "@/utils/validateFields";
-import Alumno from "@/utils/models/Alumno";
-import bcrypt from "bcrypt"
-import { NextResponse } from "next/server";
-export const POST = async (request: Request) => {
-    await connectDB();
-    const data = await request.formData()
-    console.log(data)
-    const nombre = data.get("nombre");
-    const correo = data.get("email");
-    const password = data.get("password");
-    const confirm_password = data.get("confirm_password");
-    const telefono = data.get("telefono");
-    const boleta = data.get("boleta");
-    const carrera = data.get("carrera");
-    const fields = { nombre, correo, password, confirm_password, telefono, boleta, carrera, status: 'pendiente' };
+import { connectDB } from '../../../utils/mongoose'
+import validateFields from '@/utils/validateFields'
+import Alumno from '@/utils/models/Student'
+import bcrypt from 'bcrypt'
+import { NextResponse } from 'next/server'
+export const POST = async (request: Request): Promise<NextResponse> => {
+  await connectDB()
+  const data = await request.formData()
+  console.log(data)
+  const fields = {
+    nombre: data.get('nombre'),
+    correo: data.get('email'),
+    password: data.get('password'),
+    confirm_password: data.get('confirm_password'),
+    telefono: data.get('telefono'),
+    boleta: data.get('boleta'),
+    carrera: data.get('carrera')
+  }
 
-    const errores = await validateFields(fields);
+  const errores = await validateFields(fields)
 
-    if (errores) {
-        return NextResponse.json({ message: errores }, { status: 400 })
+  if (errores !== null) {
+    return NextResponse.json({ message: errores }, { status: 400 })
+  } else {
+    const salt = await bcrypt.genSalt(10)
+    if (fields.password !== null && fields.password !== undefined && typeof fields.password === 'string') {
+      const hashedPassword = await bcrypt.hash(fields.password, salt)
+      fields.password = hashedPassword
     }
+    const alumno = new Alumno(fields)
+    await alumno.save()
 
-    const salt = await bcrypt.genSalt(10);
-    if (password !== null && password !== undefined && typeof password === 'string') {
-        const hashedPassword = await bcrypt.hash(password, salt);
-        fields.password = hashedPassword;
-    }
-    const alumno = new Alumno(fields);
-    await alumno.save();
-
-    return NextResponse.json({ message: "Se ha enviado el registro para su aprobación" }, { status: 200 })
-};
+    return NextResponse.json({ message: 'Se ha enviado el registro para su aprobación' }, { status: 200 })
+  }
+}

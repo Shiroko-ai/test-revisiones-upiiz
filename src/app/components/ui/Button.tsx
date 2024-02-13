688043cfaef8b6c8359181dc2eab7573d2809b74
Shiroko <ivanpalacios2809@gmail.com>
2024-02-13 17:33:36 -0600

    Refactor Button component to use single quotes for ButtonType and remove unnecessary whitespace

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,14 +1,14 @@
-type ButtonType = "submit" | "reset" | "button";
+type ButtonType = 'submit' | 'reset' | 'button'
 
 interface Props {
-    type: ButtonType;
-    value: string;
-    url?: string;
-    onClick?: () => void;
+  type: ButtonType
+  value: string
+  url?: string
+  onClick?: () => void
 }
 
-export default function Button({ type, value, url, onClick }: Props): JSX.Element {
-    const button = (
+export default function Button ({ type, value, url, onClick }: Props): JSX.Element {
+  const button = (
         <button
             className="block w-full transform rounded-md bg-primary px-4 py-2 text-center font-medium
             capitalize tracking-wide text-white transition-colors duration-300 hover:bg-hover
@@ -18,18 +18,20 @@ export default function Button({ type, value, url, onClick }: Props): JSX.Elemen
         >
             {value}
         </button>
-    );
-    return (
+  )
+  return (
         <>
             {
-                url ? (
+                url
+                  ? (
                     <a href={url} >
                         {button}
                     </a>
-                ) : (
-                    button
-                )
+                    )
+                  : (
+                      button
+                    )
             }
         </>
-    );
+  )
 }

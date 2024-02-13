836ff2801c8e2cd750b35e3ddc4167827a7ad9fc
Shiroko <ivanpalacios2809@gmail.com>
2024-02-13 17:31:49 -0600

    WIP: Fixed Typing

--- src/app/admin/accept-users/page.tsx
+++ src/app/admin/accept-users/page.tsx
@@ -1,42 +1,42 @@
-"use client"
-import CenterContainer from "@/app/components/containers/CenterContainer"
-import Table from "@/app/components/ui/Table"
-import { ButtonType } from "../../../../types/types"
-import { useState } from "react"
-import { useEffect } from "react"
-import Navbar from "@/app/components/ui/Navbar"
+'use client'
+import CenterContainer from '@/app/components/containers/CenterContainer'
+import Table from '@/app/components/ui/Table'
+import { type ButtonType } from '../../../../types/types'
+import { useState, useEffect } from 'react'
+
 const actions = [
-    {
-        value: "Aceptar",
-        onClick: () => {
-            console.log("Accept")
-        },
-        type: "button" as ButtonType
+  {
+    value: 'Aceptar',
+    onClick: () => {
+      console.log('Accept')
     },
-    {
-        value: "Rechazar",
-        onClick: () => {
-            console.log("Reject")
-        },
-        type: "button" as ButtonType
-    }
+    type: 'button' as ButtonType
+  },
+  {
+    value: 'Rechazar',
+    onClick: () => {
+      console.log('Reject')
+    },
+    type: 'button' as ButtonType
+  }
 ]
-export default function AcceptUsersPage() {
-    const [users, setUsers] = useState()
-    useEffect(() => {
-        const fetchUsers = async () => {
-            const response = await fetch("http://localhost:3000/api/pending-users")
-            const data = await response.json()
-            setUsers(data.data)
-        }
-        fetchUsers()
-    }, [])
-    return (
+export default function AcceptUsersPage (): JSX.Element {
+  const [users, setUsers] = useState<Array<Record<string, unknown>>>()
+  useEffect(() => {
+    const fetchUsers = async (): Promise<void> => {
+      const response = await fetch('http://localhost:3000/api/pending-users')
+      const data = await response.json()
+      setUsers(data.data as Array<Record<string, unknown>>)
+    }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchUsers()
+  }, [])
+  return (
         <>
-            <Navbar />
+
             <CenterContainer>
                 <Table data={users} actions={actions} name="Alumnos" />
             </CenterContainer>
         </>
-    )
+  )
 }

f6b22995c39aa7555473eb3ffdba147ba9dc217f
Shiroko <ivanpalacios2809@gmail.com>
2024-02-14 10:37:04 -0600

    Switched Alumno.js to Student.ts

---
+++ src/utils/models/Student.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+import { nanoid } from 'nanoid'
+
+const { Schema, model, models } = mongoose
+const StudentSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    default: () => nanoid(10)
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  ticket_number: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  carreer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Career'
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'active'],
+    required: true
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+export default models.Student || model('Student', StudentSchema)

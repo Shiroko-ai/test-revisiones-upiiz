f1ae999ab1ab1b5bcaf8fe389c64b749f31f4413
Shiroko <ivanpalacios2809@gmail.com>
2024-02-14 10:36:13 -0600

    Added validations to Teachers

--- src/utils/validateFields.ts
+++ src/utils/validateFields.ts
@@ -1,63 +1,88 @@
-import Alumno from "./models/Alumno"
+import Student from './models/Student'
+import Carreer from './models/Carreer'
 import validator from 'validator'
-type FormData = {
-    nombre: string | null;
-    correo: string | null;
-    password: string | null;
-    confirm_password: string | null;
-    telefono: string | null;
-    boleta: string | null;
-    carrera: string | null;
-};
+import Teacher from './models/Teacher'
+interface FormData {
+  nombre: string | null
+  correo: string | null
+  password: string | null
+  confirm_password: string | null
+  telefono: string | null
+  boleta: string | null
+  carrera: string | null
+}
 const friendlyNames: Record<keyof FormData, string> = {
-    nombre: "Nombre",
-    correo: "Correo electrónico",
-    password: "Contraseña",
-    confirm_password: "Confirmar contraseña",
-    telefono: "Telefono",
-    boleta: "Boleta",
-    carrera: "Carrera",
-};
-export default async function validateFields(fields: Record<string, unknown>): Promise<string | null> {
-    const errors: string[] = []
-    let first_void_name: Boolean = false
-    for (const field in fields) {
-        if (fields[field] === null || fields[field] === '' || fields[field] === undefined) {
-            if (first_void_name === false) {
-                errors.push('Los siguientes campos deben ser llenados:' + friendlyNames[field as keyof FormData])
-                first_void_name = true
-            }
-            else {
-                errors.push(friendlyNames[field as keyof FormData]);
-            }
-        }
+  nombre: 'Nombre',
+  correo: 'Correo electrónico',
+  password: 'Contraseña',
+  confirm_password: 'Confirmar contraseña',
+  telefono: 'Telefono',
+  boleta: 'Boleta',
+  carrera: 'Carrera'
+}
+export default async function validateFields (FIELDS: Record<string, unknown>, USER_TYPE: string): Promise<string | null | undefined> {
+  const errors: string[] = []
+  let FIRST_VOID_NAME: boolean = false
+  for (const field in FIELDS) {
+    if (FIELDS[field] === null || FIELDS[field] === '' || FIELDS[field] === undefined) {
+      if (!FIRST_VOID_NAME) {
+        errors.push('Los siguientes campos deben ser llenados:' + friendlyNames[field as keyof FormData])
+        FIRST_VOID_NAME = true
+      } else {
+        errors.push(friendlyNames[field as keyof FormData])
+      }
     }
-    if (errors.length > 0) {
-        return errors.join(', ')
-    }
-    if (fields['password'] !== fields['confirm_password']) {
-        errors.push('las contraseñas no coinciden')
-    }
-    if (!validator.isEmail(fields['correo'] as string)) {
-        errors.push('El correo no es valido')
-    }
-    if (!validator.isMobilePhone(fields['telefono'] as string)) {
-        errors.push('El telefono no es valido')
-    }
-    if (!validator.isNumeric(fields['boleta'] as string)) {
-        errors.push('La boleta no es valida')
-    }
-    if (!validator.matches(fields['nombre'] as string, /^[a-zA-Z\u00C0-\u00FF. ]+$/)) {
-        errors.push('El nombre solo debe contener letras o espacios')
-    }
-    const correo_repetido = await Alumno.findOne({ correo: fields['correo'] })
-    if (correo_repetido) {
-        errors.push('El correo ya existe')
-    }
-    const boleta_repetida = await Alumno.findOne({ boleta: fields['boleta'] })
-    if (boleta_repetida) {
-        errors.push('La boleta ya existe')
-    }
-    if (errors.length === 0) return null
+  }
+  if (errors.length > 0) {
     return errors.join(', ')
+  }
+  if (FIELDS.password !== FIELDS.confirm_password) {
+    errors.push('las contraseñas no coinciden')
+  }
+  if (!validator.isStrongPassword(FIELDS.password as string)) {
+    errors.push('La contraseña debe de tener por lo menos 8 caracteres, una minúscula, una mayúscula, un número y un símbolo')
+  }
+  const REPEATED_EMAIL = await Promise.all([Student.findOne({ email: FIELDS.email }), Teacher.findOne({ email: FIELDS.email })])
+
+  if (REPEATED_EMAIL !== undefined || REPEATED_EMAIL !== null) {
+    errors.push('El correo ya existe')
+  }
+  if (USER_TYPE === 'STUDENT') {
+    const carreers = await Carreer.find({}, { _id: 1 })
+    if (!validator.isEmail(FIELDS.email as string)) {
+      errors.push('El correo no es valido')
+    }
+    if (!validator.matches(FIELDS.name as string, /^[a-zA-Z\u00C0-\u00FF. ]+$/)) {
+      errors.push('El nombre solo debe contener letras o espacios')
+    }
+    if (!validator.isMobilePhone(FIELDS.phone as string)) {
+      errors.push('El telefono no es valido')
+    }
+    if (!validator.isNumeric(FIELDS.ticket_number as string)) {
+      errors.push('La boleta no es valida')
+    }
+
+    const TICKET_NUMBER = await Student.findOne({ boleta: FIELDS.boleta })
+    if (TICKET_NUMBER !== null || TICKET_NUMBER !== undefined) {
+      errors.push('La boleta ya existe')
+    }
+    if (!carreers.includes(FIELDS.career)) {
+      errors.push('La carrera no existe')
+    }
+    return errors.join(', ')
+  } else if (USER_TYPE === 'teacher') {
+    if (!validator.isNumeric(FIELDS.numero_empleado as string)) {
+      errors.push('El número de empleado debe tener sólo números')
+    }
+    if (!validator.isEmail(FIELDS.correo_institucional as string)) {
+      errors.push('El correo institucional no es válido')
+    }
+
+    if (!validator.isEmail(FIELDS.correo_electronico as string)) {
+      errors.push('El correo electrónico no es válido')
+    }
+    if (!validator.isMobilePhone(FIELDS.telefono as string)) {
+      errors.push('El teléfono proporcionado no es válido')
+    }
+  }
 }

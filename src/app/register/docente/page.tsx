b437a046cf98db5e09382ba68fefb638ff0b3be5
Shiroko <ivanpalacios2809@gmail.com>
2024-02-14 10:34:13 -0600

    Translated names to english

--- src/app/register/docente/page.tsx
+++ src/app/register/docente/page.tsx
@@ -1,54 +1,51 @@
-import CenterContainer from "@/app/components/containers/CenterContainer"
-import Form from "@/app/components/ui/Form"
-import Input from "@/app/components/ui/Input"
-import Select from "@/app/components/ui/Select"
-import Button from "@/app/components/ui/Button"
-export default function RegisterDocentePage(): JSX.Element {
-    return (
+import CenterContainer from '@/app/components/containers/CenterContainer'
+import Form from '@/app/components/ui/Form'
+import Input from '@/app/components/ui/Input'
+import Select from '@/app/components/ui/Select'
+import Button from '@/app/components/ui/Button'
+export default function RegisterDocentePage (): JSX.Element {
+  return (
         <CenterContainer>
             <Form
                 title="Registro de docentes"
                 url="/api/register-teacher"
 
             >
-                <Input type="text" name="nombre" placeholder="Nombre" required />
+                <Input type="text" name="name" placeholder="Nombre" required />
                 <Input
                     type="text"
-                    name="numeroEmpleado"
+                    name="employeeName"
                     placeholder="Número de empleado"
                     required
                     maxlength="10"
                 />
                 <Input
                     type="email"
-                    name="correoInstitucional"
+                    name="institutionalEmail"
                     placeholder="Correo institucional"
                     required
                 />
                 <Input type="email" name="correo" placeholder="Correo" required />
                 <Input
                     type="password"
-                    name="contraseña"
+                    name="password"
                     placeholder="Contraseña"
                     required
                 />
                 <Input
                     type="text"
-                    name="telefono"
+                    name="phone"
                     placeholder="Teléfono"
                     required
                 />
-                <Input
-                    type="text"
-                    name="academia"
-                    placeholder="Academia"
-                    required
-                />
+                <Select name="academy" label='Academia' id='academy' >
+                    <option value="Sistemas">Sistemas</option>
+                    <option value="Ambiental">Ambiental</option>
+                    <option value="Matemáticas">Matemáticas</option>
+                </Select>
+
                 <Button type="submit" value="Registrarse" />
             </Form>
         </CenterContainer>
-    )
-
-
+  )
 }
-

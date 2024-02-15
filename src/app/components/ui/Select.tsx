7bbb3e03b65da783dc31d99a6621981458e3f33a
Shiroko <ivanpalacios2809@gmail.com>
2024-02-14 18:48:55 -0600

    Refactor Select component

--- src/app/components/ui/Select.tsx
+++ src/app/components/ui/Select.tsx
@@ -1,25 +1,27 @@
-type SelectProps = {
-    label: string;
-    id: string;
-    children: React.ReactNode;
-    name: string;
-    [key: string]: any;
+interface SelectProps {
+  label: string
+  id: string
+  children: React.ReactNode
+  name: string
+  [key: string]: any
 }
 
-export default function Select({ label, id, children, name, ...rest }: SelectProps) {
-    return (
-        <>
+export default function Select ({ label, id, children, name, ...rest }: SelectProps): JSX.Element {
+  return (
+        <div>
             <label htmlFor={id}
-                className="block text-sm font-medium text-primary mb-2">
+                className="block text-sm font-medium text-gray-900">
                 {label}
             </label>
+            <div className="mt-2">
             <select id={id}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-         focus:ring-primary focus:border-primary block w-full p-2.5 
+         focus:ring-primary focus:border-primary block w-full p-2.5
           mb-6" name={name} defaultValue={''}>
                 <option value="" disabled>Selecciona una opción</option>
                 {children}
             </select>
-        </>
-    )
+            </div>
+        </div>
+  )
 }

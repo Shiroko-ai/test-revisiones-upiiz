075d7008b998e80e5ac4a687af2b2722d955b334
Shiroko <ivanpalacios2809@gmail.com>
2024-02-14 18:46:11 -0600

    WIP: Fixing send data from API to client

--- src/app/api/pending-users/route.ts
+++ src/app/api/pending-users/route.ts
@@ -3,9 +3,21 @@ import setFriendlyHeadings from '@/utils/setFriendlyHeadings'
 import Student from '@/utils/models/Student'
 import { NextResponse } from 'next/server'
 export async function GET (request: Request): Promise<NextResponse> {
-  await connectDB()
-  const pendingStudents: Array<Record<string, string>> = await Student.find({ status: 'pending' }, { password: 0, __v: 0, _id: 0 })
-  const newData = setFriendlyHeadings(pendingStudents)
-  console.log(newData)
-  return NextResponse.json({ newData }, { status: 200 })
+  try {
+    await connectDB()
+    const pendingStudents: Array<Record<string, any>> = await Student.find({ status: 'pending' }, { password: 0, __v: 0, _id: 0 }).lean().populate('career')
+    const filteredStudents = pendingStudents.map(student => {
+      return {
+        ...student,
+        career: student.career.name
+      }
+    })
+    console.log(filteredStudents)
+    const newData = setFriendlyHeadings(filteredStudents)
+    console.log(newData)
+    return NextResponse.json({ newData }, { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: 'Error' }, { status: 500 })
+  }
 }

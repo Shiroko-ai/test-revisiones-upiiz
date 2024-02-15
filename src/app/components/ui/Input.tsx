5610248abcd9ff108ac2ce63945477417d0d4023
Shiroko <ivanpalacios2809@gmail.com>
2024-02-14 18:47:11 -0600

    Refactor Input style

--- src/app/components/ui/Input.tsx
+++ src/app/components/ui/Input.tsx
@@ -32,36 +32,25 @@ export default function Input ({ type, placeholder, name, ...rest }: InputProps)
     }
   }
   return (
-    <div className="relative h-11 w-full min-w-[200px] mb-6">
-      {error && <p className={`absolute bottom-[-20px] left-0 text-xs text-red-500 ${lato.className}`}>Correo inválido</p>}
-      <input
-        className="peer h-full w-full rounded-md border border-blue-gray-200 bg-transparent
-     px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0
-     transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200
-     placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary
-     focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-        placeholder=" "
-        type={type}
-        name={name}
-        value={value}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        {...rest}
-      />
-      <label className={`before:content[' '] after:content[' '] pointer-events-none absolute left-0
-  -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400
-  transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block
-  before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200
-  before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block
-  after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200
-  after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1]
-  peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent
-  peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight
-   peer-focus:text-red-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary
-    peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-red-900 peer-disabled:text-transparent
-    peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 ${lato.className}`}>
-        {placeholder}
-      </label>
-    </div>
+            <div>
+            <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">
+              {placeholder}
+            </label>
+            <div className="mt-2">
+              <input
+                id={name}
+                name={name}
+                type={type}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
+                ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary
+                sm:text-sm sm:leading-6"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={value}
+                {...rest}
+              />
+            </div>
+          </div>
   )
 }

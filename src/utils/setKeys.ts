e7e7e936640716c574de3ebd7165d505399ba6d5
Shiroko <ivanpalacios2809@gmail.com>
2024-02-15 14:18:35 -0600

    Delete unused page component and fix type error in setKeys function

--- src/utils/setKeys.ts
+++ src/utils/setKeys.ts
@@ -1,4 +1,4 @@
-export default function setKeys (arr: Array<Record<string, string>>, key): Array<Record<string, string>> {
+export default function setKeys (arr: Array<Record<string, string>>, key: string): Array<Record<string, string>> {
   return arr.map(obj => {
     const keyValue = obj[key]
     return { ...obj, key: keyValue }

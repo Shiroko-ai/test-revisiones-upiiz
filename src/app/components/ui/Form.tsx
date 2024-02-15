f7e5eaaad055329ce7e01a8cb4e8dbc83fb0cd29
Shiroko <ivanpalacios2809@gmail.com>
2024-02-14 18:47:01 -0600

    Refactor Form styling and add IPNSvg logo

--- src/app/components/ui/Form.tsx
+++ src/app/components/ui/Form.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import IPNLogo from '../../../../public/Logo_IPN.png'
 import Image from 'next/image'
+import IPNSvg from './logos/IPNSvg'
 interface FormProps {
   title: string
   subtitle?: string
@@ -11,7 +12,7 @@ interface FormProps {
 
 export default function Form ({ title, subtitle, children, url }: FormProps): JSX.Element {
   const [message, setMessage] = useState('')
-  async function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit (e: React.FormEvent<HTMLFormElement>): Promise<void> {
     e.preventDefault()
     const formData = new FormData(e.target as HTMLFormElement)
     const response = await fetch(url, {
@@ -20,35 +21,31 @@ export default function Form ({ title, subtitle, children, url }: FormProps): JS
     })
     const data = await response.json()
     console.log(data)
-    setMessage(data.message)
+    setMessage(data.message as string)
   }
   return (
-    <div className="relative flex flex-col rounded-xl bg-transparent bg-clip-border
- text-gray-700 shadow-lg px-52 py-16 justify-center items-center content-center ">
-      <div className="grid grid-cols-2 gap-4">
-        <div className="col-span-1">
-          <h4 className="block font-sans text-2xl font-semibold
-      leading-snug tracking-normal text-blue-gray-900 antialiased">
-            {title}
-          </h4>
-          {subtitle && (
-            <h3 className="block font-sans text-xl  leading-snug tracking-normal
-            text-blue-gray-300 antialiased">
-              {subtitle}
-            </h3>
-          )}
-
-        </div>
-        <div className="col-span-1">
-          <Image src={IPNLogo} className="mx-auto block max-w-md" alt="VIGORE Logo" height='100' />
-        </div>
-      </div>
-      <form className="mt-4 mb-2 w-full" onSubmit={handleSubmit}>
-        <div className="mb-4 flex flex-col">
-          {children}
-        </div>
-        {message && (<p>{message}</p>)}
-      </form>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <IPNSvg
+        className="mx-auto h-20 w-auto"
+        stroke='#6c1458'
+        fill='#fff'
+      />
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        {title}
+      </h2>
     </div>
+
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" onSubmit={handleSubmit as any}>
+    {children}
+    <p className="mt-10 text-center text-sm text-gray-500">
+    {message}
+      </p>
+      </form>
+
+    </div>
+  </div>
+
   )
 }

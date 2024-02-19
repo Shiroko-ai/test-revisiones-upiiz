c6b30e31cd875551caebb9f6e0c2711924fd2642
Shiroko <ivanpalacios2809@gmail.com>
2024-02-19 11:21:22 -0600

    Add links to Navbar in AdminLayout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,17 @@
 import Navbar from '../components/ui/Navbar'
 
+const links = {
+  'Aceptar usuarios': '/admin/accept-users',
+  'Agregar carreras': '/admin/add-careers'
+}
 export default function AdminLayout ({ children }: Readonly<{ children: React.ReactNode }>): JSX.Element {
   return (
     <>
-    <Navbar />
+    <Navbar
+      links={links}
+      profilePage='/admin/profile'
+      editPage='/admin/edit-profile'
+    />
     {children}
     </>
   )

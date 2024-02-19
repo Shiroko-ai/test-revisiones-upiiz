857835db054766853455a2d60e4bc0afb249804b
Shiroko <ivanpalacios2809@gmail.com>
2024-02-19 11:23:00 -0600

    Add Teacher model and update GET and POST methods in pending-users route

--- src/app/api/pending-users/route.ts
+++ src/app/api/pending-users/route.ts
@@ -2,6 +2,7 @@ import { connectDB } from '@/utils/mongoose'
 import setFriendlyHeadings from '@/utils/setFriendlyHeadings'
 import Student from '@/utils/models/Student'
 import { NextResponse } from 'next/server'
+import Teacher from '@/utils/models/Teacher'
 export async function GET (request: Request): Promise<NextResponse> {
   try {
     await connectDB()
@@ -9,13 +10,38 @@ export async function GET (request: Request): Promise<NextResponse> {
     const filteredStudents = pendingStudents.map(student => {
       return {
         ...student,
-        career: student.career.name
+        career: student.career.name,
+        status: 'Pendiente'
       }
     })
     console.log(filteredStudents)
     const newData = setFriendlyHeadings(filteredStudents)
     console.log(newData)
-    return NextResponse.json({ newData }, { status: 200 })
+    return NextResponse.json(newData, { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: 'Error' }, { status: 500 })
+  }
+}
+
+export async function POST (request: Request): Promise<NextResponse> {
+  try {
+    await connectDB()
+    const { id } = await request.json()
+
+    const [student, teacher] = await Promise.all([
+      Student.findOne({ id }).lean().exec(),
+      Teacher.findOne({ id }).lean().exec()
+    ])
+    if (student === undefined && teacher === undefined) {
+      return NextResponse.json({ message: 'El usuario no existe o ha sido de alta previamente' }, { status: 404 })
+    }
+    if (student !== undefined) {
+      await Student.updateOne({ id }, { status: 'active' })
+    } else if (teacher !== undefined) {
+      await Teacher.updateOne({ id }, { status: 'active' })
+    }
+    return NextResponse.json({ message: 'El usuario ha sido dado de alta satisfactoriamente' }, { status: 200 })
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: 'Error' }, { status: 500 })

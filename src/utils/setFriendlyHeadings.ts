f4f3259f505a2fab6c9655ebbabf863a1a620abd
Shiroko <ivanpalacios2809@gmail.com>
2024-02-19 11:25:42 -0600

    Refactor setFriendlyHeadings to use more descriptive keys

--- src/utils/setFriendlyHeadings.ts
+++ src/utils/setFriendlyHeadings.ts
@@ -1,17 +1,20 @@
 const friendlyHeadings = {
-  correo: 'Correo',
-  nombre: 'Nombre',
-  telefono: 'Teléfono',
-  boleta: 'Boleta',
-  carrera: 'Carrera',
-  status: 'Estado'
+  email: 'Correo',
+  name: 'Nombre',
+  phone: 'Teléfono',
+  ticketNumber: 'Boleta',
+  career: 'Carrera',
+  status: 'Estado',
+  id: 'id'
 }
 export default function setFriendlyHeadings (data: Array<Record<string, string>>): Array<Record<string, string>> {
   const newData = data.map((item) => {
     console.log(item)
     const newItem: Record<string, string> = {}
     for (const key in item) {
+      console.log(key)
       newItem[(friendlyHeadings as Record<string, string>)[key]] = item[key]
+      console.log(newItem)
     }
     return newItem
   })

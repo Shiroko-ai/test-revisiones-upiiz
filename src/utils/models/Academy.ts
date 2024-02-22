3d441f6e8bfe8c0961ecca9ae3843ceafc363df7
Shiroko <ivanpalacios2809@gmail.com>
2024-02-22 14:22:59 -0600

    Add Academy model

---
+++ src/utils/models/Academy.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import { nanoid } from 'nanoid'
+
+const { Schema, model, models } = mongoose
+const AcademySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  teachers: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Teacher'
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+export default models.Academy || model('Academy', AcademySchema)

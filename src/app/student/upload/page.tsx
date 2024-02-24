3a95c4ce48434d78972bfe5b8656a44bed2c97f1
Shiroko <ivanpalacios2809@gmail.com>
2024-02-24 15:38:29 -0600

    Add form for uploading documents

--- src/app/student/upload/page.tsx
+++ src/app/student/upload/page.tsx
@@ -1,7 +1,20 @@
+import Button from '@/app/components/ui/Button'
+import DragAndDrop from '@/app/components/ui/DragAndDrop'
+import Form from '@/app/components/ui/Form'
+import Input from '@/app/components/ui/Input'
+
 export default function UploadPage (): JSX.Element {
   return (
         <div>
-        <h1>Upload Page</h1>
+        <Form title= "Subir documento" url = "/api/upload-document">
+          <Input name="name" placeholder='Nombre del documento' type='text' />
+          <DragAndDrop
+            description='Sólo se permiten archivos PDF'
+            name="file"
+            allowedTypes={['application/pdf']}
+          />
+          <Button type = "submit" value="Subir"/>
+        </Form>
         </div>
   )
 }
